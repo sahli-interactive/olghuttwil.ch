f836f1716c889c2f087f2a13367417df0b01ebc9
DoomyCrystal <shagix.ix@gmail.com>
2022-10-28 16:50:30 +0200

    Feat: Open and close mobile menu on button click

--- components/layout/navigation.js
+++ components/layout/navigation.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 import Logo from "./logo"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faXmark } from "@fortawesome/pro-regular-svg-icons"
+import { faBars, faXmark } from "@fortawesome/pro-regular-svg-icons"
 import { StoryblokComponent } from "@storyblok/react"
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 function NavLink({ blok, currentStory }) {
     return (
@@ -18,12 +20,36 @@ function NavLink({ blok, currentStory }) {
 }
 
 function Navigation({ global, currentStory }) {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const router = useRouter()
+
+    useEffect(() => {
+		router.events.on('routeChangeComplete', () => setIsOpen(false))
+		return () => {
+			router.events.off('routeChangeComplete', () => setIsOpen(false))
+		}
+	})
     return (
-        <nav className="top-0 right-0 left-0 py-6 flex flex-col justify-between items-left gap-6 bg-white border-blue-700 hover:border-blue-500 h-fit md:h-14 md:h-24 px-6 md:px-12 mx-6 border-x-4 absolute z-20">
-        <div className="flex flex-row justify-between items-center">  
-            <a href="../home"><Logo /></a>
-
-            <div className="hidden md:flex flex-row justify-between items-center gap-6">
+        <nav className="absolute top-0 left-0 right-0 mx-6 border-x-4 border-blue-700 bg-white shadow-xl z-20">
+            <div className="relative flex flex-row justify-between items-center p-6 md:px-12 bg-white z-10">
+                <a href="../home">
+                    <Logo />
+                </a>
+                <button onClick={() => setIsOpen(!isOpen)} className="md:hidden relative w-6 h-6 inline-flex justify-center items-center">
+                    <span className="sr-only">{isOpen ? 'Navigation schliessen' : 'Navigation öffnen'}</span>
+                    <FontAwesomeIcon icon={isOpen ? faXmark : faBars} className="h-6" />
+                </button>
+                <div className="hidden md:flex justify-between items-center gap-6">
+                    {global.content.nav_links?.map(blok => (
+                        <NavLink key={blok._uid} blok={blok} currentStory={currentStory} />
+                    ))}
+                    {global.content.nav_button.map(blok => (
+                        <StoryblokComponent key={blok._uid} blok={blok} />
+                    ))}
+                </div>
+            </div>
+            <div className={`absolute flex md:hidden flex-col justify-between gap-6 -left-1 -right-1 p-6 border-x-4 border-blue-700 bg-white transform transition-transform duration-300${isOpen ? ' translate-y-0' : ' -translate-y-full'}`}>
                 {global.content.nav_links?.map(blok => (
                     <NavLink key={blok._uid} blok={blok} currentStory={currentStory} />
                 ))}
@@ -31,24 +57,6 @@ function Navigation({ global, currentStory }) {
                     <StoryblokComponent key={blok._uid} blok={blok} />
                 ))}
             </div>
-
-
-
-            <button onClick={() => setIsOpen(false)} className="md:hidden relative">
-                <span className="sr-only">Schliessen</span>
-                <FontAwesomeIcon icon={faXmark} className="h-6" />
-            </button>
-
-</div>
-            <div className="flex md:hidden flex-col justify-between items-left gap-6">
-                {global.content.nav_links?.map(blok => (
-                    <NavLink key={blok._uid} blok={blok} currentStory={currentStory} />
-                ))}
-                {global.content.nav_button.map(blok => (
-                    <StoryblokComponent key={blok._uid} blok={blok} />
-                ))}
-            </div>
-
         </nav>
     )
 }
