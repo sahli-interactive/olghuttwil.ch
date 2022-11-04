f05f254db515b708c1480ca907190ddf194f0c63
DoomyCrystal <shagix.ix@gmail.com>
2022-11-04 11:34:34 +0100

    Bugfix: Prevent line break in button

--- components/bloks/news_teaser.js
+++ components/bloks/news_teaser.js
@@ -1,12 +1,9 @@
 import { useContext } from "react"
 import { NewsContext } from "../../pages/[...slug]"
 import { StoryblokComponent } from "@storyblok/react"
-import { useRouter } from "next/router"
-import Overlay from "./overlay"
 
 function NewsTeaser({ blok }) {
     const news = useContext(NewsContext)
-    const router = useRouter()
     return (
         <section className="mt-16 col-start-1 grid grid-cols-12 gap-4 md:gap-6">
             {blok.headline && <h2 className="col-start-1 md:text-3xl">{blok.headline}</h2>}
@@ -15,9 +12,11 @@ function NewsTeaser({ blok }) {
                     <StoryblokComponent key={blok._uid} blok={blok} />
                 ))}
             </div>
-            {blok.button && blok.button.map(blok => (
-                <StoryblokComponent key={blok._uid} blok={blok} />
-            ))}
+            <div className="col-span-full">
+                {blok.button && blok.button.map(blok => (
+                    <StoryblokComponent key={blok._uid} blok={blok} />
+                ))}
+            </div>
         </section>
     )
 }
