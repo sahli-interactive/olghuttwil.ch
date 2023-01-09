96f4fa5ef4ac3d21af780bfd221814d8f5437e5d
lorisryser3 <loris@sahli.io>
2023-01-09 09:04:02 +0100

    refactor info-card in Home

--- components/bloks/infobox.js
+++ components/bloks/infobox.js
@@ -6,7 +6,7 @@ function Infobox({ blok }) {
     return (
         <>
             <div className="grid grid-cols-12 gap-4">
-                <div className="col-span-12 md:col-span-6 mt-6 relative bg-yellow rounded-br-50 z-10 p-7 md:p-8 flex flex-col">
+                <div className="md:col-start-3 col-span-12 md:col-span-8 mt-6 relative bg-yellow rounded-br-50 z-10 p-7 md:p-8 flex flex-col">
                     <div className="mb-6">
                         {blok.name && <h3 className="md:text-2xl mb-3 font-bold">{blok.name}</h3>}
                         {blok.text && (
@@ -19,7 +19,7 @@ function Infobox({ blok }) {
                         <StoryblokComponent key={blok._uid} blok={blok} />
                     ))}
                 </div>
-                <div className="mt-6 col-span-12 md:col-span-6 grid md:grid-cols-6 h-min gap-4">
+                <div className="md:col-start-3 mt-6 col-span-12 md:col-span-8 grid md:grid-cols-6 h-min gap-4">
                     {blok.event_card && blok.event_card.map(blok => (
                         <StoryblokComponent key={blok._uid} blok={blok} />
                     ))}
